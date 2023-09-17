c6e1fdd5dca4a22f5e8c9dbc4c166701c2033d26
Sen <szhang@ennead.com>
2023-09-17 18:12:24 -0400

    update

--- train.py
+++ train.py
@@ -42,7 +42,7 @@ class WorkoutGuide:
         for group in today_workout:
             for exercise, duration in group:
                 self.workout_timer(duration, exercise, placeholder)
-            self.rest_timer(40, placeholder)
+                self.rest_timer(40, placeholder)
 
         st.write("Well done, you've crushed it today!")
         self.speak("Well done, you've crushed it today!")
@@ -51,14 +51,14 @@ class WorkoutGuide:
     def workout_timer(self, duration, exercise, placeholder):
         self.speak(f"Start {exercise}")
         for sec in range(duration, 0, -1):
-            placeholder.markdown(f"<h1 style='font-size:48px;'>{sec} seconds remaining.</h1>", unsafe_allow_html=True)
+            placeholder.markdown(f"<h1 style='font-size:48px;'>{exercise}\n{sec} seconds.</h1>", unsafe_allow_html=True)
             time.sleep(1)
         self.speak("Time's up! Next one.")
 
     def rest_timer(self, duration, placeholder):
         self.speak(f"Rest for {duration} seconds")
         for sec in range(duration, 0, -1):
-            placeholder.markdown(f"<h1 style='font-size:48px;'>{sec} seconds remaining.</h1>", unsafe_allow_html=True)
+            placeholder.markdown(f"<h1 style='font-size:48px;'>Take a rest!\n{sec} seconds.</h1>", unsafe_allow_html=True)
             time.sleep(1)
         self.speak("Rest time's over! Get ready.")
 
